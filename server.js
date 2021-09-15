c2ad7273bdcff110b48cd836f9b178b4cf58b1a3
SamCloutier07 <minefan07@hotmail.com>
2021-09-15 09:05:48 -0400

    Maths

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const hostname = '127.0.0.1'; // Synonyme de localhost
-const port = 2357;
+const port = 5000;
 const server = require('./route.js');
 server.listen(port, hostname, () => {
     console.log('Serveur en exécution sur http://' + hostname + ':' + port + '/');
